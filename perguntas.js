6671557638f4b96170635cbd2b0bcf8e590ce7c4
luiz0511 <luiz.santos.nunes@escola.pr.gov.br>
2025-08-13 08:09:15 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'luta',
+    'quais são as 8 armas do muay thai?',
+    'São os cotovelos,joelhos,punhos e as canelas'
+)
+
+criaCartao(
+    'luta',
+    'Qual a função do prajied na graduação do muay thai?',
+    'Muitos lutadores usam como se fosse uma proteção no ringue alem de ser uma forma de hierarquia'
+)
+
+criaCartao(
+    'futebol',
+    'quantos jogadores tem em uma partida de futebol?',
+    'em uma partida de futebol a 24 jogadores'
+)
+
+criaCartao(
+    'futebol',
+    'qual e o maior time do Brasil?',
+    'no brasil o maior time e o São Paulo FC sendo pai do corintians, palmeiras e santos'
+)
