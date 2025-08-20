1df5977d3b301013c136d9f9d6ba8df3c669ac43
luiz0511 <luiz.santos.nunes@escola.pr.gov.br>
2025-08-20 08:15:17 -0300

    ultimas modificaçoes

--- perguntas.js
+++ perguntas.js
@@ -21,3 +21,15 @@ criaCartao(
     'qual e o maior time do Brasil?',
     'no brasil o maior time e o São Paulo FC sendo pai do corintians, palmeiras e santos'
 )
+
+criaCartao(
+    'futebol',
+    'quando foi conquistado o primeiro mundial do São Paulo FC ?', 
+    'O primeiro mundial do são paulo veio em 1992 sendo conquistado no japão vencendo o barcelona por 2X1'
+)
+
+    criaCartao(
+    'futebol',
+    'qual e o maior artilheiro do São Paulo?',
+    'o maior artilheiro do são paulo foi serginho chulapa com 242 gols em sua passagem pelo são paulo '
+)
